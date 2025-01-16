36cc46157e417234c0e663609f243ed3978bfe62
Anish Wanare <106481793+Anishwanare@users.noreply.github.com>
2025-01-16 20:43:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-    origin: ['http://localhost:5173' || 'https://imagifyyyy.netlify.app'],
+    origin: [process.env.FRONTEND_URL ||'http://localhost:5173' || 'https://imagifyyyy.netlify.app'],
     methods: ["POST", "PUT", "DELETE", "GET"],
     credentials: true,
     optionsSuccessStatus: 200,
